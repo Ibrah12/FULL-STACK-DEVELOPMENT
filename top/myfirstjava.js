c7848162719cd48a5fb5f81cbf45349676107b70
Ibrah12 <93139017+Ibrah12@users.noreply.github.com>
2023-06-13 03:58:41 +0300

    Add files via upload

---
+++ top/myfirstjava.js
@@ -0,0 +1,8 @@
+function here(){
+    document.getElementById("bbb").remove()
+    document.getElementById("xtd").innerHTML="I believe you should hire me because I can take my time and design your website precisely and ensure all elements work as needed.Besides,I am willing to take directions and research and apply customized features."
+}
+function more(){
+    document.getElementById("ccc").remove()
+    document.getElementById("xtd3").innerHTML="I am an expert based in Kenya, Nairobi city, with 4 years of experience in the field.I have interacted with various environments of user interfaces including school resource planning system(ERP'S)"
+}
